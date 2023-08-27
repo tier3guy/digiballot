4ad8d37ca0d00e73df2e65e564103ff957d64f67
Avinash Gupta <avinash.gupta@vils.ai>
2023-08-27 23:11:32 +0530

    Configuring Tailwind -- avinash.gupta

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+// Internal Imports
+import { Suspense } from "react";
+
 // External Imports
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -7,13 +10,19 @@ import routes from "./routes";
 const App = () => {
   return (
     <Router>
-      <Routes>
-        {routes.map((route, index) => {
-          return (
-            <Route path={route.path} key={index} element={<route.element />} />
-          );
-        })}
-      </Routes>
+      <Suspense>
+        <Routes>
+          {routes.map((route, index) => {
+            return (
+              <Route
+                path={route.path}
+                key={index}
+                element={<route.element />}
+              />
+            );
+          })}
+        </Routes>
+      </Suspense>
     </Router>
   );
 };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 const Home = () => {
-  return <p>Home</p>;
+  return <div className="bg-slate-800 text-white">Home</div>;
 };
 
 export default Home;
